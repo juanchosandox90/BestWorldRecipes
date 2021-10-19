4fba239fad547b2e932c164202910889331e1d17
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-10-19 14:03:25 -0500

    Return BottomSheet Safeargs
     - Added to navigation graph inside BottomSheet the safe arg to check if apply button is pressed to return the values from the chips
     - Make a new request if Apply button is pressed from the Bottom Sheet fragment

--- app/src/main/java/com/sandoval/bestworldrecipes/ui/fragments/RecipesFragment.kt
+++ app/src/main/java/com/sandoval/bestworldrecipes/ui/fragments/RecipesFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sandoval.bestworldrecipes.R
 import com.sandoval.bestworldrecipes.viewmodels.MainViewModel
@@ -22,6 +23,8 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args by navArgs<RecipesFragmentArgs>()
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
@@ -69,7 +72,7 @@ class RecipesFragment : Fragment() {
     private fun readDatabase() {
         lifecycleScope.launch {
             mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
-                if (database.isNotEmpty()) {
+                if (database.isNotEmpty() && !args.backFromBottomSheet) {
                     mAdapter.setData(database[0].foodRecipe)
                     hideShimmerEffect()
                 } else {
