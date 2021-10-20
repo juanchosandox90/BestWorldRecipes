3c464975e8e547fefc69b54521fc0f31ed67ceb8
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-10-20 17:46:39 -0500

    View Pager Adapter and Details Fragments
     - Create the ViewPagerAdapter.kt to handle the fragments that will be allocated inside the details activity
     - Create the 3 fragments that will be allocated inside the DetailsActivity.kt: OverviewFragment.kt, IngredientsFragment.kt and InstructionsFragment.kt
     - Implement the ViewPagerAdapter.kt inside DetailsActivity.kt and initialize the tab layout with the viewpager adapter to navigate through the fragments
     - Fix a UI bug related to activity_details.xml

---
+++ app/src/main/java/com/sandoval/bestworldrecipes/ui/fragments/viewpagerfragments/InstructionsFragment.kt
@@ -0,0 +1,20 @@
+package com.sandoval.bestworldrecipes.ui.fragments.viewpagerfragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sandoval.bestworldrecipes.R
+
+class InstructionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    }
+
+}
