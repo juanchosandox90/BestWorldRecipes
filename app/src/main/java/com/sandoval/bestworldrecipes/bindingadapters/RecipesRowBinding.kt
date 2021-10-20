8feaab997e53c7b17261f416557b8a8d16538ace
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-10-20 17:20:57 -0500

    Details Activity Design
      - DetailsActivity.kt and activity_details.xml created
      - go to details activity binding function created inside of RecipesRowBinding.kt to navigate to Details Activity through data binding feature
      - Make ExtendedIngredient.kt and Result.kt classes extend from parcelable and annotate them with @Parcelize to pass in the future all the information in safe args through navGraph feature.
      - Create custom styles for the details activity components to be used in the future
      - Fix some warnings regarding accesibility inside recipes_row_layout.xml

--- app/src/main/java/com/sandoval/bestworldrecipes/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/sandoval/bestworldrecipes/bindingadapters/RecipesRowBinding.kt
@@ -1,12 +1,18 @@
 package com.sandoval.bestworldrecipes.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.sandoval.bestworldrecipes.R
+import com.sandoval.bestworldrecipes.data.models.Result
+import com.sandoval.bestworldrecipes.ui.fragments.RecipesFragmentDirections
+import java.lang.Exception
 
 class RecipesRowBinding {
 
@@ -67,6 +73,20 @@ class RecipesRowBinding {
             imageView.load(imageUrl) {
                 crossfade(600)
                 error(R.drawable.ic_error_placeholder)
+            }
+        }
+
+        @BindingAdapter("android:onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.e("Exception: ", e.toString())
+                }
             }
         }
 
