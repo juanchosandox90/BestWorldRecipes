67faf8ca438b1832990a9cc29a1ee78f69a80818
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-10-22 18:48:10 -0500

    Recipes Dao Database
     - Refactor RecipesDao.kt and added methods for Favorites Recipes
     - Refactor RecipesDatabase.kt and added the entity of FavoritesEntity.kt
     - Fix UI text tabs lowercase

--- app/src/main/java/com/sandoval/bestworldrecipes/data/database/dao/RecipesDao.kt
+++ app/src/main/java/com/sandoval/bestworldrecipes/data/database/dao/RecipesDao.kt
@@ -1,9 +1,7 @@
 package com.sandoval.bestworldrecipes.data.database.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.sandoval.bestworldrecipes.data.database.entity.FavoritesEntity
 import com.sandoval.bestworldrecipes.data.database.entity.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -15,4 +13,16 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoritesRecipes()
 }
