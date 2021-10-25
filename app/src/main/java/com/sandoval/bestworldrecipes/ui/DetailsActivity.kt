35b70d0440f895f3aa88936ef8f91867bba9a89d
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-10-25 07:00:22 -0500

    Save Favorite Recipe
      - Modify MainViewModel.kt private methods to public, to be used inside DetailsActivity.kt
      - Created inside DetailsActivity.kt saveRecipeToFavorites method to save a recipe inside the favorites_recipe_table into the DB
      - Created method to change the start icon of the favorite action to yellow, when a favorite recipe is saved inside the DB
      - Create a method to show a snackbar message when a favorite recipe is saved inside the DB
      - Create some specific strings.xml to be used in the save favorite recipe feature

--- app/src/main/java/com/sandoval/bestworldrecipes/ui/DetailsActivity.kt
+++ app/src/main/java/com/sandoval/bestworldrecipes/ui/DetailsActivity.kt
@@ -4,20 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.sandoval.bestworldrecipes.R
 import com.sandoval.bestworldrecipes.adapters.ViewPagerAdapter
+import com.sandoval.bestworldrecipes.data.database.entity.FavoritesEntity
 import com.sandoval.bestworldrecipes.ui.fragments.viewpagerfragments.IngredientsFragment
 import com.sandoval.bestworldrecipes.ui.fragments.viewpagerfragments.InstructionsFragment
 import com.sandoval.bestworldrecipes.ui.fragments.viewpagerfragments.OverviewFragment
 import com.sandoval.bestworldrecipes.utils.Constants.Companion.RECIPE_RESULT_KEY
+import com.sandoval.bestworldrecipes.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_details.*
 
+@AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
 
     private val args by navArgs<DetailsActivityArgs>()
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,8 +60,36 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) finish()
+        if (item.itemId == android.R.id.home) {
+            finish()
+        } else if (item.itemId == R.id.save_to_favorites_menu) {
+            saveRecipeToFavorites(item)
+        }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveRecipeToFavorites(item: MenuItem) {
+        val favoritesEntity = FavoritesEntity(
+            0,
+            args.result
+        )
+        mainViewModel.insertFavoriteRecipes(favoritesEntity)
+        changeMenuItemColor(item, R.color.yellow)
+        showSnackBar(applicationContext.getString(R.string.saved_favorite_recipe_snackbar_message))
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(
+            detailsLayout,
+            message,
+            Snackbar.LENGTH_LONG
+        ).setAction(applicationContext.getString(R.string.saved_favorite_recipe_snackbar_action)) {}
+            .show()
+    }
+
+    private fun changeMenuItemColor(item: MenuItem, color: Int) {
+        item.icon.setTint(ContextCompat.getColor(this, color))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
