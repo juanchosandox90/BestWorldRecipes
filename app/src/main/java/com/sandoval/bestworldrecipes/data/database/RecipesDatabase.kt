ad8a14261ebaa234746ba435c6cf2a70d37e9d46
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-11-06 11:12:52 -0500

    Food Joke Offline Caching
      - Added to the DAO the methods to be used inside the LocalDataSource.kt
      - Added to the LocalDataSource.kt.kt the methods related to get the food joke from the DAO
      - Added to the MainViewModel.kt the variables to get the food joke from the LocalDataSource.kt
      - Added to the MainViewModel.kt the methods to get the food joke from the repository and assigned to the food joke response variable
      - Added to the MainViewModel.kt the method to cache the data from the LocalDataSource.kt

--- app/src/main/java/com/sandoval/bestworldrecipes/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/sandoval/bestworldrecipes/data/database/RecipesDatabase.kt
@@ -5,14 +5,15 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.sandoval.bestworldrecipes.data.database.dao.RecipesDao
 import com.sandoval.bestworldrecipes.data.database.entity.FavoritesEntity
+import com.sandoval.bestworldrecipes.data.database.entity.FoodJokeEntity
 import com.sandoval.bestworldrecipes.data.database.entity.RecipesEntity
 
 //I normal circumstances version should be upgraded everytime a new entity is added to the DB
 //as this is a development project if an error occurs this could be fixed by unistalling the app
 //and installing it again
 @Database(
-    entities = [RecipesEntity::class, FavoritesEntity::class],
-    version = 1,
+    entities = [RecipesEntity::class, FavoritesEntity::class, FoodJokeEntity::class],
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(RecipesTypeConverter::class)
